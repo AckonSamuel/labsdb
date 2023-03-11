45fcd296f89eb533ed1cc6164fb8f5b02bc1d34d
Ackon Samuel <ackons433@gmail.com>
2023-03-11 03:20:15 +0000

    Reset secrets

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -312,7 +312,7 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
   config.jwt do |jwt|
-    jwt.secret = ENV['JWT_SECRET_KEY']
+    jwt.secret = Rails.application.credentials.jwt_secret_key
     jwt.dispatch_requests = [
       ['POST', %r{^/login$}]
     ]
