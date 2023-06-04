63e4e4d540fecb23ff7c0988eedafbce132848ee
Ackon Samuel <ackons433@gmail.com>
2023-06-04 20:02:49 +0000

    Hot fix

--- config/environment.rb
+++ config/environment.rb
@@ -5,15 +5,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  #   domain: 'gmail.com',
-  user_name: 'labsdb',
-  password: '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',
-  authentication: :plain,
-  enable_starttls_auto: true
-}
