aaaaea935180986e4c905e162bc8fcfd444a7a09
Ackon Samuel <ackons433@gmail.com>
2023-07-08 08:16:05 +0000

    Create migrations for instrument_labs

---
+++ db/migrate/20230706200454_create_instrument_labs.rb
@@ -0,0 +1,10 @@
+class CreateInstrumentLabs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :instrument_labs, id: :uuid do |t|
+      t.references :instrument, null: false, foreign_key: true, type: :uuid
+      t.references :lab, null: false, foreign_key: true, type: :uuid
+
+      t.timestamps
+    end
+  end
+end
