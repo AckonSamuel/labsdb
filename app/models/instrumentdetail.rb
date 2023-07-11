666ba22ff02b1406dabb46596bdcefb39700d501
Ackon Samuel <ackons433@gmail.com>
2023-07-11 14:24:05 +0000

    Update instrument serializer file

--- app/models/instrumentdetail.rb
+++
@@ -1,3 +0,0 @@
-class Instrumentdetail < ApplicationRecord
-    belongs_to :instruments
-end
--- app/serializers/instrument_serializer.rb
+++ app/serializers/instrument_serializer.rb
@@ -1,3 +1,11 @@
 class InstrumentSerializer < ActiveModel::Serializer
-  attributes :id, :instrument_name, :manufacturing_year, :number_of_devices, :description, :price, :model
+  attributes :id, :instrument_name, :manufacturing_year, :number_of_devices, :description, :price, :model, :labs, :categories
+
+  def categories
+    object.categories.pluck(:id)
+  end
+
+  def labs
+    object.labs.pluck(:id)
+  end
 end
