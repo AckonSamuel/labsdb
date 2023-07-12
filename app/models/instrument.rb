2b4dbdc4581828116365c1ff37a5b6c70e306a97
Ackon Samuel <ackons433@gmail.com>
2023-07-12 23:53:31 +0000

    update

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,6 +1,13 @@
 class Instrument < ApplicationRecord
+    has_many_attached :instrument_photos, dependent: :destroy
     has_many :instrument_categories
     has_many :categories, through: :instrument_categories
     has_many :instrument_labs
     has_many :labs, through: :instrument_labs
+
+    def instrument_photos_url
+        return [] unless instrument_photos.attached?
+
+        instrument_photos.attachments.map(&:url)
+    end
 end
