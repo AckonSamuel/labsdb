62a6b6844a4c4f1be7973b68400dda3d73a29623
Ackon Samuel <ackons433@gmail.com>
2023-07-12 23:53:54 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :instrumentdetails
   resources :labs
   resources :instruments
+  resources :users do
+    get :profile, on: :collection
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
