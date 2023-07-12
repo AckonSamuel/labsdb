97604357d3c0bb1995c0b8e8b47d86ae4cb49b81
Ackon Samuel <ackons433@gmail.com>
2023-07-12 23:53:38 +0000

    update

--- app/serializers/instrument_serializer.rb
+++ app/serializers/instrument_serializer.rb
@@ -1,5 +1,5 @@
 class InstrumentSerializer < ActiveModel::Serializer
-  attributes :id, :instrument_name, :manufacturing_year, :number_of_devices, :description, :price, :model, :labs, :categories
+  attributes :id, :instrument_name, :manufacturing_year, :number_of_devices, :description, :price, :model, :labs, :categories, :instrument_photos_url
 
   def categories
     object.categories.pluck(:id)
