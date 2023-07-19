be67547e0112ab1b605b371056acb8d573f8e32d
Ackon Samuel <ackons433@gmail.com>
2023-07-19 17:11:03 +0000

    upgrades

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
-class Category < ApplicationRecord
-    has_many :instruments
+class Category < ApplicationRecord 
+    has_many :instrument_categories, dependent: :delete_all
+    has_many :instruments, through: :instrument_categories
 end
--- app/models/lab.rb
+++ app/models/lab.rb
@@ -1,3 +1,4 @@
 class Lab < ApplicationRecord
-    has_many :instruments
+    has_many :instrument_labs, dependent: :delete_all
+    has_many :instruments, through: :instrument_labs
 end
